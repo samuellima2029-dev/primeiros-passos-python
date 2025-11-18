fe7396e4d07707abd5ba38b4803af34dd1cb16b9
Samuel Lima <samuel.lima2029@gmail.com>
2025-11-18 15:22:55 -0300

    Add files via upload

---
+++ exercio05.py
@@ -0,0 +1,7 @@
+n1 = int(input('Digite um numero:'))
+n2 = int(input('Digite outro numero:'))
+soma = n1 + n2
+subt = n1 - n2
+mult = n1 * n2
+div = n1 / n2
+print('O resultado da soma é:',soma , 'sudtracao:', subt , 'multiplicacao:', mult, 'e divisao:', div)
